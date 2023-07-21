3b2345f3cda2bff54ee90021097f301d3dc280ed
panwar2001 <ayushpanwar691@gmail.com>
2023-07-22 02:37:06 +0530

    add real time paragraph generator

--- Backend/index.js
+++ Backend/index.js
@@ -1,31 +1,40 @@
 const express=require('express');
+const cors=require("cors");
 const app=express();
+app.use(express.json());
+app.use(cors({origin: '*'}));
 const http=require('http');
+const {generate}=require('./randomWords');
 const server=http.createServer(app);
 const {Server}=require('socket.io');
 const io=new Server(server,{cors:{origin:'*'}});
+
 require('dotenv').config();
 const PORT=process.env.PORT|8080;
 
 const easy = io.of('/easy');
 const medium = io.of('/medium');
 const hard = io.of('/hard');
-let remainingTime = 61; 
+let remainingTime = 91; 
 
 let easyLevelData={}, mediumLevelData={}, hardLevelData={};
+let easyParagraph,mediumParagraph,hardParagraph;
+let startTime=0;
 
 setInterval(() => {
-    remainingTime=remainingTime-1>=0?remainingTime-1:60;
-    if(remainingTime==60){
+    remainingTime=remainingTime-1>=0?remainingTime-1:90;
+    if(remainingTime==90){
       easyLevelData={};
       mediumLevelData={};
       hardLevelData={};
+      easyParagraph=generate(70)
+      mediumParagraph=generate(100);
+      hardParagraph=generate(120);
     }
       easy.emit("players_update",remainingTime,easyLevelData);
       medium.emit("players_update",remainingTime,mediumLevelData);
       hard.emit("players_update",remainingTime,hardLevelData);
   }, 1000); 
-
 easy.on('connection', (socket) => {
    socket.on('playerInfo', (name,speed) => {
     easyLevelData[socket.id]={
@@ -52,5 +61,14 @@ hard.on('connection', (socket) => {
   });
 });
 
-
+app.get("/easyParagraph",(req,res)=>{
+  res.json({paragraph:easyParagraph});  
+})
+app.get("/mediumParagraph",(req,res)=>{
+  res.json({paragraph:mediumParagraph});
+ })
+ app.get("/hardParagraph",(req,res)=>{
+  res.json({paragraph:hardParagraph});
+ })
+  
 server.listen(PORT,()=>console.log(`server started listening on port ${PORT}`));
