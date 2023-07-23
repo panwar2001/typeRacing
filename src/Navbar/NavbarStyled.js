dd45624c7e763581d9fb010b0309c7c0b455854a
Ayush Negi <96509253+negia7265@users.noreply.github.com>
2023-07-24 01:20:10 +0530

    Update NavbarStyled.js

--- src/Navbar/NavbarStyled.js
+++ src/Navbar/NavbarStyled.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 export const NavbarContainer = styled.div`
-  width: 95%;
+  width: 97%;
   height: 80px;
   border-radius: 20px;
-  background-color: #181b1f;
+  background-color: #36454f;
   display: flex;
   justify-content: space-around;
   align-items: center;
-  color: #f0f6fc;
+  color: white;
   margin-top: 5px;
 `;
 export const StyledUl = styled.ul`
@@ -20,12 +20,12 @@ export const StyledUl = styled.ul`
   font-size: 20px;
 `;
 export const StyledA = styled.a`
-  color: #f0f6fc;
+  color: white;
   text-decoration: none;
   transition: color 0.3s;
   :hover {
     cursor: pointer;
-    color: #b4bbc2;
+    color: black;
     text-decoration: none;
   }
 `;
