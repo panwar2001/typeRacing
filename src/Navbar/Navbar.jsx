b5d838ac8441548ec877923b724671dc2caefe12
Ayush Negi <96509253+negia7265@users.noreply.github.com>
2023-07-23 19:46:16 +0530

    Update Navbar.jsx

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavbarContainer, StyledUl, StyledA } from "./NavbarStyled";
 const navLinks = ["quit","switch to another game"];
 const Navbar= () => (
+  <div style={{ display: "flex", justifyContent: "center" }}>
   <NavbarContainer>
     <h3> ⌨ typeRacing </h3>
     <StyledUl>
@@ -12,6 +13,7 @@ const Navbar= () => (
       ))}
     </StyledUl>
   </NavbarContainer>
+    </div>
 );
 
 export default Navbar;
